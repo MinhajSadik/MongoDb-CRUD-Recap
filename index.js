34d4f0ed7e90ab5ddbf017fad7ae74bf60f719dd
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-04-04 18:26:53 +0600

    half working done

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 const fruit = {
     product: 'banana',
     price: '323'
 }
 const users = ['Anika', 'minhaj', 'sadik', 'hamida', 'sharmin', 'ikra', 'rumana', 'susmita', 'samiya'];
 
-app.get('/user/:id', (req, res) => {
-
+app.get('/users/:id', (req, res) => {
     const id = req.params.id;
     const name = users[id]
-    res.send(name);
-    })
+    // res.send(name);
+    res.send({ id, name });
+    // res.send(req.query.name)
+})
+    
+
+// app.get('/', (req, res) => {
+//     res.sendFile(`${__dirname}/index.html`);
+// })
 app.get('/', (req, res) => {
     res.send(fruit)
 })
