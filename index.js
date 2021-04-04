33c45579ad274fbddedc3084d9597b7e7d6637f1
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-04-04 17:08:00 +0600

    recap runnig

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
 const app = express();
-
+const fruit = {
+    product: 'banana',
+    price: '323'
+}
 app.get('/', (req, res) => {
-    res.send('express node setup successfully')
+    res.send(fruit)
 })
 
 app.listen(545, console.log('port on 545'))
