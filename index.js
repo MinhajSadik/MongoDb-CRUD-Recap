3d005677eb26b7c0ec0693b39115b5a1527af309
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-04-04 17:21:35 +0600

    dynamic api

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const fruit = {
     product: 'banana',
     price: '323'
 }
+const users = ['Anika', 'minhaj', 'sadik', 'hamida', 'sharmin', 'ikra', 'rumana', 'susmita', 'samiya'];
+
+app.get('/user/:id', (req, res) => {
+
+    const id = req.params.id;
+    const name = users[id]
+    res.send(name);
+    })
 app.get('/', (req, res) => {
     res.send(fruit)
 })
