f182df313c64e6e6296b58bb30671a4d7028c597
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-04-06 09:16:55 +0600

    setup done

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 import express from 'express';
 import cors from 'cors';
+import MongoClient from 'mongodb';
+
+const password = 'MongoDB1';
+
+const uri = "mongodb+srv://MinhajSadik:MongoDB1@cluster0.iojzl.mongodb.net/MongoDb-Recap?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//     res.sendFile(`${__dirname}/index.html`);
+
+
+// const users = ['Anika', 'minhaj', 'sadik', 'hamida', 'sharmin', 'ikra', 'rumana', 'susmita', 'samiya'];
+
+// app.get('/users/:id', (req, res) => {
+//     const id = req.params.id;
+//     const name = users[id]
+//     // res.send(name);
+//     res.send({ id, name });
+//     // res.send(req.query.name)
 // })
 
-const users = ['Anika', 'minhaj', 'sadik', 'hamida', 'sharmin', 'ikra', 'rumana', 'susmita', 'samiya'];
-
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const name = users[id]
-    // res.send(name);
-    res.send({ id, name });
-    // res.send(req.query.name)
-})
-
-app.get('/furits/banana', (req, res) => {
-    res.send({fruit: 'banana', quantity: 100, price: '400TK'});
-})
+// app.post('/addUser', (req, res) => {
+//     res.send(req.body)
+// })
 
 app.get('/', (req, res) => {
     res.send("Thank You Guys For Coming>")
 })
 
-app.post('/addUser', (req, res) => {
-    //save to database
-    // const user = req.body;
-    // user.id = 545;
-    res.send(req.body)
-})
+client.connect(err => {
+    const collection = client.db("MongoDb-Recap").collection("Project's");
+    app.get('/', (req, res) => {
+        collection.InsertOne({
+
+        })
+    })
+});
 
 app.listen(545, console.log('port on 545'))
